e0d8d7d9881c9369373c276bdd488a2149219534
Andrew Sayre <andrew@sayre.net>
2018-03-25 12:58:49 -0500

    CoreOS configuration test

---
+++ coreos/ignition.js
@@ -0,0 +1,20 @@
+{
+  "ignition": {
+    "config": {},
+    "timeouts": {},
+    "version": "2.1.0"
+  },
+  "networkd": {},
+  "passwd": {
+    "users": [
+      {
+        "name": "core",
+        "sshAuthorizedKeys": [
+          "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAhkd3JyxNUQ+wWnwkH/HvfCFAkQLLlR6fMn5rPiKIfRf0ZEpJit01oGrnZXCQJcgLefq/5EU3NMdGV6TW8g7RUlN7jbGQ6hVVfF9XTtAEmz51ERoNE7TtfB090sRqRwJVoN/Ah7HzJkGO3LZarfIPr9WAkqoYF1CuzZ61v68zsgCQEDZCd2Gl5WHPYWtpyG17FNQcXNX8mXBX77LG5K7yyBs9xewBNa2KEIdUB4Nez/ScLbymjPIdHGrpBraPbX+9ofbgIyg/ZOwsG5HSshuzL7s3L487yURCAPqtO+dELZC+K61LeBnPNQB/HEQXI/F0Ab9UPL+WsM+kqil7Egc+Dw== rsa-key-20180325"
+        ]
+      }
+    ]
+  },
+  "storage": {},
+  "systemd": {}
+}
